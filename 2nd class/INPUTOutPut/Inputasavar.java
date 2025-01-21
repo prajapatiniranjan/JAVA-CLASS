d3c4fc2a597ef895820cf16fec57cdc2632573cc
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2025-01-21 13:57:57 +0530

    Input/Output

---
+++ 2nd class/INPUTOutPut/Inputasavar.java
@@ -0,0 +1,15 @@
+
+import java.utill.Scanner;
+class Inputasavar
+{
+public static void main(String ars[])
+{
+
+int a;
+System.out.println("Enter an number");
+Scanner obj=new Scanner(System.in);
+a=obj.nextInt();
+System.out.println(a);
+}	
+
+}
---
+++ 2nd class/INPUTOutPut/One.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class One
+{
+
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+{
+ String name;
+name=obj.nextLine();
+System.out.println(name);
+
+}
+}
+}
