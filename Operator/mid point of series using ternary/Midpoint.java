89b9972fd5f7a19ff103a28457ffc67caf3db479
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2025-01-25 13:50:56 +0530

    Mid point find out

---
+++ Operator/mid point of series using ternary/Midpoint.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Midpoint{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        
+        int num1 = 2;
+        int num2 = 4;
+        int num3 = 6;
+        int num4 = 8;
+        
+    System.out.println((num2!=0 && num3!=0)?(num2+num3)/2:"wrong input");
+        
+    }
+}
