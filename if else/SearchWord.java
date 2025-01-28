3c988a69cd49d7201f2daa0c78c545899ecfabd0
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2025-01-28 21:33:17 +0530

    if else or Search word char location

--- if else/SearchWord.java
+++ if else/SearchWord.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class SearchWord {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        String word;
+        char letter;
+
 
         System.out.println("Enter a Word:");
-        String inputString = obj.nextLine();
+        word=obj.nextLine();
+        System.out.println("Enter a Word to search:");
+        letter=obj.nextLine().charAt(0);
+        if(word.charAt(0)==letter)
+        {
+            System.out.println("letter found" + " " +letter);
+        }
+        else{
+            System.out.println("letter not found" );
+
+        }
+        
 
 
     }
